06c248b471b8f06032323275fd84ae979f709c4c
mertfilizaygit <mertfilizay@gmail.com>
2023-09-30 16:06:44 +0300

    trial

--- main.py
+++ main.py
@@ -3,6 +3,18 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
+import spacy
 
-a = 5
-b = 100
+#name of the english language model
+nlp = spacy.load("en_core_web_sm")
+
+def tokenize(text):
+    doc = nlp(text)
+    return [token.text for token in doc]
+
+with open('trial.txt', 'r') as file:
+    text = file.read()
+
+
+tokens = tokenize(text)
+print(tokens)
